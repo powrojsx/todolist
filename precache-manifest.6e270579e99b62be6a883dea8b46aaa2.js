8ca616f62cfc0efe1190f48f764d8d277b497369
Kamil Powroźnik <kpowroznik.dev@gmail.com>
2019-04-09 21:31:59 +0200

    Updates

---
+++ precache-manifest.6e270579e99b62be6a883dea8b46aaa2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "367b1464b3deb5653ef7",
+    "url": "/todolist/static/js/main.ff4f0d7a.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "fb8e0bcabce415e18a27",
+    "url": "/todolist/static/js/2.998918eb.chunk.js"
+  },
+  {
+    "revision": "cf8743f7e8aa0ecbe56e7f16a9d8715c",
+    "url": "/todolist/index.html"
+  }
+];
